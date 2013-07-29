230ab35d36aa3352a38efe8f731acd7e80d1195b
Moonshiness554 <sasha.jaffarove@gmail.com>
2013-07-29 12:06:49 -0700

    Create led line.ino

---
+++ led line.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  for (int i=2; i<=13; i++)
+  {
+  pinMode(i,OUTPUT);
+  }
+  
+}
+
+void loop()
+{
+  for (int i=2; i<=13; i++)
+  {
+    digitalWrite(i, HIGH);
+    delay (100);
+    digitalWite(i,LOW);
+  }
+  
+}
+  
